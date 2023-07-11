dc9afb1647413a20f5a72a7f9a88af5b422a3704
Abhishek Sharma <96715256+Abhishek0145@users.noreply.github.com>
2023-07-11 20:59:48 +0530

    Add files via upload

---
+++ Python Basics.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("hello word")
+
+
+# In[2]:
+
+
+Print("hello word")
+
+
+# In[4]:
+
+
+print(1.2)
+
+
+# ### ;wsjansnandkn
+# 
+
+# ####;kahshwkekkqw
+# 
+
+# #####;khsdjshd
+
+# ##;jasajhqwhm
+
+# In[5]:
+
+
+x = "abhishek"
+type(x)
+
+
+# In[7]:
+
+
+x = False
+type(x)
+
+
+# In[8]:
+
+
+x = print("Abhishek")
+type(x)
+y = print(19)
+type (y)
+
+
+# In[11]:
+
+
+x = "ABhishek"
+print(x)
+type(x)
+
+
+# In[10]:
+
+
+y = 19
+print(y)
+type(y)
+
+
+# In[ ]:
+
+
+
+
